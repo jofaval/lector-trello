0b48c25f8d174bb36b9771080f0e1bd8e9cce4d6
pepe_fabra_valverde <jofaval@outlook.com>
2021-04-30 23:17:47 +0200

    Docked lector

--- scripts/js/lector.js
+++ scripts/js/lector.js
@@ -1,3 +1,9 @@
+/**
+ * Lee el contenido de un fichero dada la ruta
+ * 
+ * @param {string} ruta 
+ * @returns object
+ */
 export const leerContenidoDelFichero = async ruta => {
     const content = await fetch(ruta)
         .then(onLecturaFichero)
@@ -8,12 +14,25 @@ export const leerContenidoDelFichero = async ruta => {
     return content;
 }
 
+/**
+ * Se ejecuta cuando se ha terminado de leer el fichero,
+ * lo parsea a JSON
+ * 
+ * @param {*} _res 
+ * @returns object
+ */
 const onLecturaFichero = async _res => {
     // console.log('respuesta', _res);
 
     return _res?.json().then(data => data)
 }
 
+/**
+ * Se ejecuta cuando ocurre un error en la lectura del fichero
+ * 
+ * @param {object} _res 
+ * @returns void
+ */
 const onLecturaFicheroError = _res => {
     // console.error('error respuesta', _res);
 }
