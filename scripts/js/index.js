6da3d5b4f91bb029885c64a0628f5cb6ae241203
pepe_fabra_valverde <jofaval@outlook.com>
2021-04-30 20:28:52 +0200

    Create render

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,13 +1,17 @@
 import { leerContenidoDelFichero } from "./lector.js";
 import { filename } from "./config.js";
 import parse from "./parser.js";
+import render from "./render.js";
 
 const init = async () => {
     let result = await leerContenidoDelFichero(filename);
     console.log('og result', result);
     
-    let parsed = parse(result)
+    let parsed = parse(result);
     console.log('resultado', parsed);
+
+    let render = render(parsed);
+    console.log('renderizado', render);
 }
 
 init();
