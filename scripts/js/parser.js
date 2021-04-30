a22dddcf80ac1c1dda63f9220ea100adb7fc1adf
pepe_fabra_valverde <jofaval@outlook.com>
2021-04-30 20:26:15 +0200

    Create parseChecklist

--- scripts/js/parser.js
+++ scripts/js/parser.js
@@ -1,5 +1,6 @@
 export const parse = data => {
     const labels = parseLabels(data)
+    const checklists = parseChecklist(data)
 
     return {
         'labels': labels,
@@ -10,6 +11,20 @@ const parseLabels = data => {
     return data?.labels?.map((label) => {
         const { name, color, id } = label;
         return { name, color, id }
+    })
+}
+
+const parseChecklist = data => {
+    return data?.checklists?.map((checklist) => {
+        const { name, id, idCard, pos, checkItems } = checklist
+
+        const mappedCheckItems = checkItems.map(checkItem => {
+            const { id, name, post, state } = checkItem;
+
+            return { id, name, post, state: state == 'complete' }
+        })
+
+        return { id, idCard, name, pos, checkItems: mappedCheckItems }
     })
 }
 
