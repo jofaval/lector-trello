bad0be6568a881baf1fb78af14753054321fdce7
pepe_fabra_valverde <jofaval@outlook.com>
2021-04-30 22:47:33 +0200

    Create renderBackgroundImage

--- scripts/js/render.js
+++ scripts/js/render.js
@@ -1,5 +1,10 @@
 export const render = data => {
-    
+    const backgroundImage = renderBackgroundImage(data)
+}
+
+const renderBackgroundImage = data => {
+    const { bgImage } = data;
+    document.body.style.backgroundImage = `url("${bgImage}")`;
 }
 
 export default render
