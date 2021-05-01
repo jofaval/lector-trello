1e5162e65859f7c57e9ba5bb1ec89779949a2590
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-01 18:11:05 +0200

    Create renderAttachments

--- src/components/card-modal.js
+++ src/components/card-modal.js
@@ -26,16 +26,27 @@ const renderCard = json => {
     return `
         ${labels}
         <p>${marked(desc)}</p>
-        ${attachments}
+        ${json?.attachments && json?.attachments.length ? attachments : attachments}
         ${checklists}
         <div class="py-3 w-100">&nbsp;</div>
         ${comments}
     `
 }
 
-const renderAttachments = json => {
-    return ``;
-}
+const renderAttachment = ({ id, idMember, name, url, fileName, date, mimeType }) =>
+`<div class="attachment p-3 my-2" title="${name}" id="${id}">
+    <a href="${url}" class="btn text-gray" onclick="window.open('${url}', '_blank')">
+        <span class="attachment-title">${name ? name : fileName}</span>
+    </a>
+</div>`
+
+const renderAttachments = ({ attachments }) => 
+`<div class="attachments">
+    <span class="attachments-title h3">Archivos adjuntos</span>
+    <div class="attachment-files">
+        ${attachments?.map(renderAttachment).join('')}
+    </div>
+</div>`
 
 const renderChecklistItem = ({ id, name, state }) => 
 `<div class="checklist-item">
