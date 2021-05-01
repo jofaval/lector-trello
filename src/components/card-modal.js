588188db3fe566d669a7cd2f485761525b6f6a45
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-01 15:38:12 +0200

    Render labels in modal

--- src/components/card-modal.js
+++ src/components/card-modal.js
@@ -8,8 +8,18 @@ const renderCardModal = json => {
     return renderModal(json);
 }
 
+const renderLabels = ({ labels }) => {
+    const mappedLabels = labels?.map(renderLabel)?.join('')
+
+    return `<div class="labels">${mappedLabels}</div>`;
+}
+
+const renderLabel = ({ name, color }) => `<span class="badge label ${color}">${name}</span>`
+
 const renderModal = json => {
     const { name, desc } = json
+
+    const labels = renderLabels(json)
 
     return `
     <div class="modal" tabindex="-1" onclick="closeCardModal()">
@@ -20,6 +30,7 @@ const renderModal = json => {
                     <button type="button" class="btn-close" data-bs-dismiss="modal-card" onclick="closeCardModal()" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
+                    ${labels}
                     <p>${marked(desc)}</p>
                 </div>
                 <div class="modal-footer">
