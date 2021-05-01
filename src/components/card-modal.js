4f925652e55eed9f47332c27f19dd3b04773c2bc
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-01 12:50:21 +0200

    Create base card-modal renderer

---
+++ src/components/card-modal.js
@@ -0,0 +1,34 @@
+/**
+ * Representa visualmente el modal de una tarjeta
+ * 
+ * @param {object} json 
+ * @returns string
+ */
+const renderCardModal = json => {
+    return renderModal(json);
+}
+
+const renderModal = json => {
+    const { name, desc } = json
+
+    return `
+    <div class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">${name}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>${desc}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+    `
+}
