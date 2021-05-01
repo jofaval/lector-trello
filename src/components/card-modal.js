23e5e5696f3cb0f4d0b7c3a1907a79aff4b0aec5
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-01 15:16:52 +0200

    Add proper modal interaction

--- src/components/card-modal.js
+++ src/components/card-modal.js
@@ -12,18 +12,18 @@ const renderModal = json => {
     const { name, desc } = json
 
     return `
-    <div class="modal" tabindex="-1">
-        <div class="modal-dialog">
+    <div class="modal" tabindex="-1" onclick="closeCardModal()">
+        <div class="modal-dialog" onclick="stopPropagation()">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">${name}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal-card" onclick="closeCardModal()" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <p>${marked(desc)}</p>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal-card" onclick="closeCardModal()">Close</button>
                     <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
