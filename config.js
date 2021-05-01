421e7d419fc2c4f9385b8e14e6497c0bc9184673
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-01 15:59:47 +0200

    Remove config from repository

--- config.js
+++
@@ -1,6 +0,0 @@
-export const filename = '/data/FCTs 03-2021 - 06-2021.json'
-// export const filename = '/data/Orca-MarketTrack.json'
-
-console.log('filename', filename);
-
-export default filename;
