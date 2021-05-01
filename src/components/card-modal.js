47d78bb9c49bd18c603586ee1b6b2b2c39ebec8c
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-01 12:54:02 +0200

    Install marked library

--- src/components/card-modal.js
+++ src/components/card-modal.js
@@ -20,7 +20,7 @@ const renderModal = json => {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <p>${desc}</p>
+                    <p>${marked(desc)}</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
