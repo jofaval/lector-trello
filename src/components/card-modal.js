66e11481b49af49f7e926d28f96bfee446a10d8a
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-01 16:13:29 +0200

    Create renderCard

--- src/components/card-modal.js
+++ src/components/card-modal.js
@@ -16,10 +16,18 @@ const renderLabels = ({ labels }) => {
 
 const renderLabel = ({ name, color }) => `<span class="badge label ${color}">${name}</span>`
 
+const renderCard = json => {
+    const { name, desc } = json
+    const labels = renderLabels(json)
+
+    return `
+        ${labels}
+        <p>${marked(desc)}</p>
+    `
+}
+
 const renderModal = json => {
     const { name, desc } = json
-
-    const labels = renderLabels(json)
 
     return `
     <div class="modal" tabindex="-1" onclick="closeCardModal()">
@@ -30,16 +38,13 @@ const renderModal = json => {
                     <button type="button" class="btn-close" data-bs-dismiss="modal-card" onclick="closeCardModal()" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ${labels}
-                    <p>${marked(desc)}</p>
+                    ${renderCard(json)}
                 </div>
-                <div class="modal-footer">
+                <!-- div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal-card" onclick="closeCardModal()">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+                </div -->
             </div>
         </div>
     </div>
-  
     `
 }
