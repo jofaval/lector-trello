26470266f3ce5b0e77b7575ee99f1a15449dbae4
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-02 13:10:46 +0200

    Adjusted members spacing

--- src/components/member.js
+++ src/components/member.js
@@ -7,11 +7,11 @@ const radius = 30;
 
 export const renderMember = ({ id, url, fullName, initials, avatarUrl }) => 
 `<div class="member" id="${id}">
-    <a href="${url}" style="min-width: 50px" class="btn text-dark" title='${fullName}' onclick="window.open('${url}', '_blank')">
+    <a href="${url}" style="min-width: 50px" class="btn p-0 m-0 text-dark" title='${fullName}' onclick="window.open('${url}', '_blank')">
         <img class="img bg-white rounded-circle" width="${radius}" height="${radius}" src="${avatarUrl}/original.png" alt='${initials}'>
     </a>
 </div>`
 
-export const renderMembers = ({ members }) => `<div class="members d-inline-flex">${members?.map(renderMember)?.join('')}</div>`
+export const renderMembers = ({ members }) => `<div class="members mb-3 d-inline-flex">${members?.map(renderMember)?.join('')}</div>`
 
 export default renderMembers;
