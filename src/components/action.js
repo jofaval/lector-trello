c4e47bcf3aca7cd4527e6c28da86576cd2fa9255
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-02 12:49:26 +0200

    Create renderActions

---
+++ src/components/action.js
@@ -0,0 +1,68 @@
+import { renderMember } from "./member.js";
+
+/**
+ * Renderiza una sola acción
+ * 
+ * @param {object} action 
+ * @returns string
+ */
+export const renderAction = ({ id, member, type, data, date }) => {
+    let actionDetail = '';
+
+    // Si se ha encontrado texto, se usa
+    if ("text" in data) {
+        const { text } = data;
+        actionDetail = `con el contenido "${text}"`;
+    }
+
+    // Se si ha realizado dentro de una tarjeta se indica
+    let cardString = '';
+    if ("card" in data) {
+        const { card: { name: cardName } } = data;
+        cardString = `a la tarjeta "${cardName}"`
+    }
+    
+    // Se si ha realizado dentro de una lista se indica
+    let listString = '';
+    if ("list" in data) {
+        const { list: { name: listName } } = data;
+        listString = `en la lista "${listName}"`
+    }
+    
+    // Se si ha realizado dentro de una lista de tareas se indica
+    let checklistString = '';
+    if ("checklist" in data) {
+        const { checklist: { name: checklistName } } = data;
+        checklistString = `en la lista de tareas "${checklistName}"`
+    }
+    
+    // Se si ha realizado dentro de una lista de tareas se indica
+    if ("checkItem" in data) {
+        const { checkItem: { name: checkItemName, state } } = data;
+        const checkItemString = `marcado "${checkItemName}" como "${state}"`
+
+        if (checklistString) {
+            checklistString = `${checkItemString} ${checklistString}`;
+        } else {
+            checklistString = checkItemString;
+        }
+    }
+
+    const finalString = `Se ha ${type} ${actionDetail} ${cardString} ${listString} ${checklistString} el ${date}`
+
+    return `<div class="action" id="${id}">
+        ${renderMember(member)}
+        <span class="action-detial">${finalString}</span>
+    </div>`
+}
+
+/**
+ * Renderiza todas las acciones
+ * 
+ * @param {object} data 
+ * @returns string
+ */
+export const renderActions = ({ actions }) =>
+`<div class="actions">${actions?.map(renderAction).join('')}</div>`
+
+export default renderActions;
