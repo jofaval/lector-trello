4d6968c6edc7a85d494393b44c7dcec5e797048f
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-02 13:20:48 +0200

    Improved action detailing

--- src/components/action.js
+++ src/components/action.js
@@ -12,34 +12,34 @@ export const renderAction = ({ id, member, type, data, date }) => {
     // Si se ha encontrado texto, se usa
     if ("text" in data) {
         const { text } = data;
-        actionDetail = `con el contenido "${text}"`;
+        actionDetail = `con el contenido <div class="card w-100 p-3">${marked(text.replaceAll('\n', '<br />'))}</div>`;
     }
 
     // Se si ha realizado dentro de una tarjeta se indica
     let cardString = '';
     if ("card" in data) {
         const { card: { name: cardName } } = data;
-        cardString = `a la tarjeta "${cardName}"`
+        cardString = `a la tarjeta <span class="badge bg-light border text-dark" style="font-size: small;">${cardName}</span>`
     }
     
     // Se si ha realizado dentro de una lista se indica
     let listString = '';
     if ("list" in data) {
         const { list: { name: listName } } = data;
-        listString = `en la lista "${listName}"`
+        listString = `en la lista <span class="badge bg-light border text-dark" style="font-size: small;">${listName}</span>`
     }
     
     // Se si ha realizado dentro de una lista de tareas se indica
     let checklistString = '';
     if ("checklist" in data) {
         const { checklist: { name: checklistName } } = data;
-        checklistString = `en la lista de tareas "${checklistName}"`
+        checklistString = `en la lista de tareas <span class="badge bg-light border text-dark" style="font-size: small;">${checklistName}</span>`
     }
     
     // Se si ha realizado dentro de una lista de tareas se indica
     if ("checkItem" in data) {
         const { checkItem: { name: checkItemName, state } } = data;
-        const checkItemString = `marcado "${checkItemName}" como "${state}"`
+        const checkItemString = `marcado <span class="badge bg-light border text-dark" style="font-size: small;">${checkItemName}</span> como <span class="badge bg-light border text-dark" style="font-size: small;">${state}</span>`
 
         if (checklistString) {
             checklistString = `${checkItemString} ${checklistString}`;
@@ -48,13 +48,17 @@ export const renderAction = ({ id, member, type, data, date }) => {
         }
     }
 
-    const finalString = `Se ha ${type} ${actionDetail} ${cardString} ${listString} ${checklistString} el ${date}`
+    const finalString = `Se ha <span class="font-weight-bold">${type}</span> ${actionDetail} ${cardString} ${listString} ${checklistString}`
 
     return `<div class="action d-flex m-2 p-3" id="${id}">
         <div class="action-creator">
             ${renderMember(member)}
         </div>
-        <span class="action-detial">${finalString}</span>
+        <span class="action-detial">
+            <i class="action-detail-date">${date}</i>
+            <br />
+            <span class="action-detail-content">${finalString}</span>
+        </span>
     </div>`
 }
 
