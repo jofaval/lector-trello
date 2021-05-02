a52e46d8284a555fa4636b8977974ac08a0706c8
pepe_fabra_valverde <jofaval@outlook.com>
2021-05-02 13:08:36 +0200

    Add radius to images

--- src/components/member.js
+++ src/components/member.js
@@ -1,7 +1,14 @@
+/**
+ * El radio de la imagen
+ * 
+ * @var int
+ */
+const radius = 30;
+
 export const renderMember = ({ id, url, fullName, initials, avatarUrl }) => 
 `<div class="member" id="${id}">
     <a href="${url}" style="min-width: 50px" class="btn text-dark" title='${fullName}' onclick="window.open('${url}', '_blank')">
-        <img class="img bg-white rounded-circle" width="25" height="25" src="${avatarUrl}/original.png" alt='${initials}'>
+        <img class="img bg-white rounded-circle" width="${radius}" height="${radius}" src="${avatarUrl}/original.png" alt='${initials}'>
     </a>
 </div>`
 
